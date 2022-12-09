a666f0f9765964ec23a4462d5cb0a6fab5e0ca93
salahi salim <salahi.salim@outlook.com>
2022-12-09 14:35:57 +0000

    frequency

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,17 @@
 
 def frequencies(items):
     frequencies = {}
+    for x in range(len(items)):
+	    items[x] = str(items[x])
+    for x in range(len(items)):
+        item = items[x]
+        count = 0
+        for each in items:
+            if each == item:
+                count += 1
+        frequencies[item] = count
     # Your code goes here
     return frequencies
+
+
+
